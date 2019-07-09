d49dbc2df07e111293104210329586ac9cd91753
anisidrenmouche <anisidrenmouche@hotmail.fr>
2019-07-10 00:02:59 +0200

    cours php

---
+++ DEMO.php
@@ -0,0 +1,29 @@
+<?php
+//les variables en Php permettent de sauvegarder temporairement les valeurs en mémoire
+// le signe = lui assigne une valeur
+// chaine de caractere ' " pour les additionner on appelle cela de la concatenation
+// Seulement les guillemets double " peuvent interpole les variables "$prenom"
+// booléen ce sont des variables qui permettent de savoir ci cela est vrai ou faux true faulse
+
+
+
+
+//
+
+//$prenom = 'marc';
+//$nom = 'pis';
+//$note1 = 10;
+//$note2 = 20;
+//$moyenne = (($note1 + $note2) /2) ;
+//echo ' bonjour ' . $prenom. ' '  . $nom. ' vous avez eu ' . (($note1 + $note2) /2) . ' de moyenne ';
+//echo "bonjour $prenom $nom vous avez eu $moyenne de moyenne";
+
+//
+
+
+
+
+
+
+
+?>
