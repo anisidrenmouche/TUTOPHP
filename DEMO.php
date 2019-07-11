6daf893d522b4470b8620921436ac2b54b4055a6
anisidrenmouche <anisidrenmouche@hotmail.fr>
2019-07-11 12:40:04 +0200

    mis a jour

--- DEMO.php
+++ DEMO.php
@@ -54,20 +54,105 @@
 
 // exemple de tableau classe d'école
 
-$classe =[
-    [
-        'nom' => 'Anis',
-        'prenom' => 'idren',
-        'notes' => [10, 12, 14]
+//$classe =[
+    //[
+        //'nom' => 'Anis',
+        //'prenom' => 'idren',
+      //  'notes' => [10, 12, 14]
 
-    ],
-    [
-        'nom' => 'Raju',
-        'prenom' => 'Kumar',
-        'notes' => [22, 12, 22]
+    //],
+    //[
+        //'nom' => 'Raju',
+      //  'prenom' => 'Kumar',
+    //    'notes' => [22, 12, 22]
 
-    ],
-];
-echo $classe[1]['notes'][1];
+  //  ],
+//];
+//echo $classe[1]['notes'][1];
+
+
+
+// Les conditions
+/*
+$note = (int)readline('Entre ta note la tu crois quoi');
+if($note >=10){
+    echo 'YUHOUUU TU AS LA MOYENNE ';
+
+}  elseif ($note== 10){
+    echo 'TU AS PEINE LA MOYENNE JUSTE';
+}
+ else{
+    echo 'A MERDE UNE PROCHAINE FOIS ';
+}
+*/
+
+
+// Exemple 1 pas forcement le meilleur
+
+/*
+$action = (int)readline('Entre ta note la tu crois quoi : (1: attaquer, 2: defendre, 3: passer mon tour)');
+if($action === 1){
+    echo 'attaque ';
+
+}  elseif ($action=== 2){
+
+    echo 'defense';
+
+}  elseif ($action === 3){
+
+    echo 'operation non recconue';
+}
+else{
+    echo 'A MERDE tu as ecrit quoi ';
+}
+*/
+
+// Exemple 2 le plus souvent utilisé
+
+
+/*
+$action = (int)readline('Entre ta note la tu crois quoi : (1: attaquer, 2: defendre, 3: passer mon tour)');
+
+switch ($action){
+    case 1:
+        echo 'j\attaque';
+        break;
+    case 2:
+        echo 'je defends';
+        break;
+    case 3:
+        echo 'walou';
+        break;
+    default:
+        echo 'heinnn';
+}
+*/
+
+
+$heure = (int)readline('entrez une heure :');
+if ((8<= $heure && $heure <= 12) || (14 <= $heure && $heure <= 18)){
+    echo 'le magasin est ouvert';
+} else {
+    echo 'le magasin est fermé';
+}
+
+/*
+Vrai  && vrai = vrai
+Vrai  && faux = faux
+faux  && vrai = faux
+
+
+Vrai  || vrai = vrai
+Vrai  || faux = vrai
+faux  || vrai = faux
+
+
+// pour inverser une condition point !
+
+
+
+
+
+
 
 
